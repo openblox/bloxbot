423dd90ada8109000b62b168f1d3855576b06798
John M. Harris, Jr <johnmh@openblox.org>
2017-03-09 22:34:38 -0500

    Add base commands plugin

---
+++ plugins/cmd/cmd.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2017 John M. Harris, Jr. <johnmh@openblox.org>
+ *
+ * This file is part of bloxbot.
+ *
+ * bloxbot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bloxbot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bloxbot.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <plugin.h>
+#include <internal.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <signal.h>
+
+void bloxbot_plugin_cmd_init(bloxbot_Plugin* plug){
+	//Register core commands here
+}
+
+void bloxbot_plugin_cmd_deinit(){
+	//Unregister core commands here
+}
